3f9b2f8d284ac0d2f1d490ff5ee5cc39e020fa28
이진우 <came1230@naver.com>
2015-01-19 19:00:56 +0900

    test 서버 연습

---
+++ 이진우/C sharp test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace C_sharp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            string input;
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            newsock.Bind(ipep);
+            newsock.Listen(10);
+            Console.WriteLine("waiting for a client...");
+
+            Socket client = newsock.Accept();
+            IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+
+            Console.WriteLine("Conected with {0} at port {1}", clientep.Address, clientep.Port);
+        }
+    }
+}
