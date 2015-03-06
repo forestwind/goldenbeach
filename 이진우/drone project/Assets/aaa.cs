5c0378af1104f444dcd8376024c9bf30118e8cd6
이진우 <came1230@naver.com>
2015-03-06 18:38:49 +0900

    dll 파일 유니티에 asset 저장

---
+++ 이진우/drone project/Assets/aaa.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aaa : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
