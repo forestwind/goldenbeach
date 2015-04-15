373c183349349cdfb32090c0e794ec5a6f494d1f
이진우 <came1230@naver.com>
2015-04-15 18:55:24 +0900

    합친 프로젝트 업로드

---
+++ DroneGame/Assets/Scripts/GameUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+	public Text timeText;
+	public float _time;
+
+	// Use this for initialization
+	void Start () {
+		_time = 10.0f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		_time -= Time.deltaTime;
+		timeText.text = _time.ToString ();
+		}
+
+}
