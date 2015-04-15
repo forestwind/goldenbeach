06788d61bf9112b5efbaa4ec2c5b6c1042af8108
이진우 <came1230@naver.com>
2015-04-15 15:59:42 +0900

    추가

---
+++ Project/DroneGame/Assets/bullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+	Vector3 destination;
+	public int speed=10000;
+
+	// Use this for initialization
+	void Start () {
+		destination = GameObject.Find("aim").transform.localPosition;
+		Debug.Log(destination);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (destination.normalized * Time.deltaTime * speed);
+
+
+		if(transform.position.z > 1000)
+			Destroy(this.gameObject);
+
+	}
+}
